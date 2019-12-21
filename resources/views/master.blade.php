a9ec5a71e9c8701c401d428ca7ea9911eb0f1143
Николай <talam0nal@gmail.com>
2019-12-21 15:22:09 +0300

    Мастер-шаблон

---
+++ resources/views/master.blade.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/css/app.css">
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
+	</head>
+
+	<body>
+		@yield ('content')
+	</body>
+</html>
