468b2dfe6c02b1a302fe51c2087d7396b2beeec0
Николай <talam0nal@gmail.com>
2019-12-21 15:22:29 +0300

    Шаблон для погоды

---
+++ resources/views/weather.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                test
+            </div>
+        </div>
+    </div>
+@endsection
