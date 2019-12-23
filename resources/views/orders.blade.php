3347b60de2cd1f7dd967a01fdc11cc73d2c95227
Николай <talam0nal@gmail.com>
2019-12-23 17:00:45 +0300

    Вывод таблицей списка заказов

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -4,7 +4,57 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-            	 {{ count($orders) }}
+                <table class="table table-striped">
+                    <tr>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            Партнёр
+                        </th>
+                        <th>
+                            Стоимость заказа
+                        </th>
+                        <th>
+                            Наименование состава заказа
+                        </th>
+                        <th>
+                            Статус заказа
+                        </th>
+                        <th>
+
+                        </th>
+                    </tr>
+                    @foreach ($orders as $order)
+                        <tr>
+                            <td>
+                                {{ $order->id }}
+                            </td>
+
+                            <td>
+                                {{ $order->partner->name }}
+                            </td>
+
+                            <td>
+                                Стоимость
+                            </td>
+
+                            <td>
+                                Наименование состава заказа
+                            </td>
+
+                            <td>
+                                Статус заказа
+                            </td>
+
+                            <td>
+                                <a href="#" target="_blank">
+                                    Редактировать
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
             </div>
         </div>
     </div>
